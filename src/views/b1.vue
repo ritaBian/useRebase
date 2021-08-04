a3dbffe856f14a3448351a5735c05e51064aa443
bianjunping <bianjunping@oilchem.net>
2021-08-04 16:37:45 +0800

    b1分支第一次提交

---
+++ src/views/b1.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: bjp
+ * @Description: b1
+-->
+<template>
+  <div class="b1">
+    <ul>
+      <li>怎叹那山有木兮那木有枝，心悦君兮啊君不知</li>
+      <li>怎叹</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'b1',
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  mounted () { },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
