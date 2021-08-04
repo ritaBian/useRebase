dc7f942dae565eefbafb8be709ae3581c5231460
bianjunping <bianjunping@oilchem.net>
2021-08-04 16:49:23 +0800

    b1分支的第三次提交

---
+++ src/views/b1-3.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: bjp
+ * @Description: b1分支的第三次修改
+-->
+<template>
+  <div class="b1-3">
+    这是b1分支的第三次修改哦，嘻嘻哈哈
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'b1-3',
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  mounted () { },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
